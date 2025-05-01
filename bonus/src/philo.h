817a77aa0722ca25c6c48b260395c46704747979
Tetsuro Ando <tetsuro997@gmail.com>
2025-05-02 08:08:23 +0900

    norming

--- bonus/src/philo.h
+++ bonus/src/philo.h
@@ -6,14 +6,13 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 14:51:02 by teando            #+#    #+#             */
-/*   Updated: 2025/05/02 08:05:08 by teando           ###   ########.fr       */
+/*   Updated: 2025/05/02 08:08:04 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <errno.h>
 # include <fcntl.h>
 # include <limits.h>
 # include <pthread.h>
@@ -22,9 +21,7 @@
 # include <stddef.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
 # include <sys/time.h>
-# include <sys/types.h>
 # include <sys/wait.h>
 # include <unistd.h>
 
